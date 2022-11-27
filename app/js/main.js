153b5022cc7afcb59b94d712734ad085455a4318
Rodchenko Nikita <bira10a@gmail.com>
2022-11-28 02:23:31 +0300

    added (no-full) section footer

--- app/js/main.js
+++ app/js/main.js
@@ -179,3 +179,15 @@ Sim.initialize = function (that) {
 };
 
 new Sim();
+
+$(".btn").on("click", function (event) {
+
+  event.preventDefault();
+
+  var id = $(this).attr('href'),
+
+    top = $(id).offset().top;
+
+  $('body,html').animate({ scrollTop: top }, 1500);
+});
+
